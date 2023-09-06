fd3c41d27d42cb15ff7ef38460fd3bfbf6b04c0a
Christopher <cmbush@gmail.com>
2023-09-05 18:15:40 -0700

    edited url path name error for salespeople

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     path("salespeople/", api_list_salespeople, name="api_list_salespeople"),
-    path("salesperson/<int:id>/", api_show_salesperson, name="api_show_salesperson"),
+    path("salespeople/<int:id>/", api_show_salesperson, name="api_show_salesperson"),
     path("customers/", api_list_customers, name="api_list_customers"),
     path("customers/<int:id>/", api_show_customer, name="api_show_customers"),
     path("sales/", api_list_sales, name="api_list_sales"),
