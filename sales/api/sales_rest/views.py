9434bf24c65a8c6e2507bf0cd5094f89bf9663b6
Christopher <cmbush@gmail.com>
2023-09-05 18:14:47 -0700

    commented out render

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import JsonResponse
 from .models import AutomobileVO, Sale, Salesperson, Customer
 from common.json import ModelEncoder
