0644c0d58d75a13cad2e0bef0ca841f4ee9a0413
Christopher <cmbush@gmail.com>
2023-09-05 17:18:02 -0700

    model encoders and salesperson view functions

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -6,12 +6,12 @@ from django.db import models
 
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17)
-    sold = models.BooleanField(default = False)
+    sold = models.BooleanField(default=False)
 
 
 class Salesperson(models.Model):
-    first_name = models.CharField(max_length = 200, null=True)
-    last_name = models.CharField(max_length= 200, null=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
     employee_id = models.CharField(max_length=50)
 
     def __str__(self):
@@ -19,10 +19,10 @@ class Salesperson(models.Model):
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length = 200, null=True)
-    last_name = models.CharField(max_length = 200, null=True)
-    address = models.CharField(max_length = 200, null=True)
-    phone_number = models.CharField(max_length=200, null = True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,97 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import AutomobileVO, Sale, Salesperson, Customer
+from common.json import ModelEncoder
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "vin",
+        "sold",
+    ]
+
+
+class SalespersonEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+        "id",
+    ]
+
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+        "id",
+    ]
+
+
+class SalesEncoder(ModelEncoder):
+    model = Sale
+    properties = [
+        "price",
+        "automobile",
+        "salesperson",
+        "customer",
+        "id"
+    ]
+    encoders = {
+        "automobile": AutomobileVOEncoder(),
+        "salesperson": SalespersonEncoder(),
+        "customer": CustomerEncoder(),
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_salespeople(request):
+    if request.method == "GET":
+        salespeople = Salesperson.objects.all()
+        return JsonResponse(
+            {"salespeople": salespeople},
+            encoder=SalespersonEncoder,
+            safe=False
+        )
+
+    else:
+        content = json.loads(request.body)
+        try:
+            salesperson = Salesperson.objects.create(**content)
+            return JsonResponse(
+                salesperson,
+                encoder=SalespersonEncoder,
+                safe=False,
+            )
+        except Salesperson.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid Salesperson"},
+                status=400,
+            )
+
+
+@require_http_methods(["GET", "DELETE"])
+def api_show_salesperson(request, id):
+    if request.method == "GET":
+        try:
+            salesperson = Salesperson.objects(id=id)
+            return JsonResponse(
+                {"salesperson": salesperson},
+                encoder=SalespersonEncoder,
+                safe=False,
+            )
+        except Salesperson.DoesNotExist:
+            return JsonResponse(
+                {"message": "Salesperson does not exist"},
+                status=404
+            )
+    else:
+        count, _ = Salesperson.objects.filter(id=id).delte()
+        return JsonResponse({"deleted": count > 0})
