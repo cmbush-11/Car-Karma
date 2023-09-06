cdcc0e3d7b0ff1fcfcb06b99ebea879bfcc721c8
Christopher <cmbush@gmail.com>
2023-09-06 11:16:38 -0700

    added salesperson form to app and to nav

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AddSalesperson from './SalespersonForm';
 
 function App() {
+
+
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route
+            path="salespeople/"
+            element={<AddSalesperson getSalespeople={getSalespeople} />} />
         </Routes>
       </div>
     </BrowserRouter>
