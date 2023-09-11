3a1b6ac8b7bad17a3a6ced860d086cf1ad7e07bd
Erin Gerber <erin.gerber3@gmail.com>
2023-09-11 22:55:36 +0000

    Update App.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,19 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-<<<<<<< HEAD
-import TechnicianList from './TechnicianList';
-import TechnicianForm from './TechnicianForm';
-import AppointmentForm from './AppointmentForm';
-import AppointmentList from './AppointmentList';
-import AppointmentHistoryList from './AppointmentHistory';
-import AutomobileList from './AutomobileList'
-import AutomobileForm from './AutomobileForm';
-import VehicleModelList from './VehicleModelList';
-import VehicleModelForm from './VehicleModelForm';
-import ManufacturerForm from './ManufacturerForm';
-import ManufacturerList from './ManufacturerList'
-=======
 import SalespersonForm from './SalespersonForm';
 import SalespeopleList from './SalespeopleList';
 import SalespersonHistory from './SalespersonHistory';
@@ -27,7 +14,7 @@ import VehicleModelForm from './VehicleModelForm';
 import VehicleModelList from './VehicleModelList';
 import AutoList from './InventoriedAutoList';
 import AutomobileForm from './InventoriedAutoForm';
->>>>>>> main
+
 
 function App(props) {
   return (
@@ -36,7 +23,7 @@ function App(props) {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-<<<<<<< HEAD
+
           <Route
            path="technicians">
             <Route index element={<TechnicianList/>}/>
@@ -46,24 +33,7 @@ function App(props) {
               <Route index element={<AppointmentList/>}/>
               <Route path="new" element={<AppointmentForm/>}/>
               <Route path="history" element={<AppointmentHistoryList/>}/>
-           </Route>
-           <Route path="models">
-            <Route index element={<VehicleModelList/>}/>
-            <Route path="new" element={<VehicleModelForm/>}/>
-
-          </Route>
-          <Route path="automobiles">
-            <Route index element={<AutomobileList/>}/>
-            <Route path="new" element={<AutomobileForm/>}/>
-
-          </Route>
-          <Route path="manufacturers">
-            <Route index element={<ManufacturerList/>}/>
-            <Route path="new" element={<ManufacturerForm/>}/>
-
-          </Route>
-
-=======
+              </Route>
           <Route path="/salespeople">
             <Route index element={<SalespeopleList />} />
             <Route path="salespeople/add" element={<SalespersonForm />} />
@@ -89,7 +59,7 @@ function App(props) {
             <Route index element={<AutoList />} />
             <Route path="/automobiles/add" element={<AutomobileForm />} />
           </Route>
->>>>>>> main
+
         </Routes>
       </div>
     </BrowserRouter>
