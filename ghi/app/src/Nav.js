971757e7616cfcc6bd4c05d19d333baaceb6e7bd
Christopher <cmbush@gmail.com>
2023-09-11 17:25:21 -0700

    nav links no longer active by default

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -11,29 +11,29 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className='nav-item'>
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/customers">Customers</NavLink>
+              <NavLink className="nav-link" to="/customers">Customers</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/customers/add">Add Customer</NavLink>
+              <NavLink className="nav-link" to="/customers/add">Add Customer</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/salespeople">Salespeople</NavLink>
+              <NavLink className="nav-link" to="/salespeople">Salespeople</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/salespeople/add">Add Salesperson</NavLink>
+              <NavLink className="nav-link" to="/salespeople/add">Add Salesperson</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/salespeople/history">Salesperson History</NavLink>
+              <NavLink className="nav-link" to="/salespeople/history">Salesperson History</NavLink>
             </li>
 
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/sales">Sales</NavLink>
+              <NavLink className="nav-link" to="/sales">Sales</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/sales/add">Add Sale</NavLink>
+              <NavLink className="nav-link" to="/sales/add">Add Sale</NavLink>
             </li>
              <li>
             <NavLink className="nav-link" to="/technicians">Technicians</NavLink>
@@ -51,22 +51,22 @@ function Nav() {
             <NavLink className="nav-link" to="/appointments/history/">Appointment History</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/manufacturers">Manufacturers</NavLink>
+              <NavLink className="nav-link" to="/manufacturers">Manufacturers</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/manufacturers/add">Add Manufacturer</NavLink>
+              <NavLink className="nav-link" to="/manufacturers/add">Add Manufacturer</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/models/">Vehicle Models</NavLink>
+              <NavLink className="nav-link" to="/models/">Vehicle Models</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/models/add">Add Vehicle Model</NavLink>
+              <NavLink className="nav-link" to="/models/add">Add Vehicle Model</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/automobiles/">Auto Inventory</NavLink>
+              <NavLink className="nav-link" to="/automobiles/">Auto Inventory</NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink className="nav-link active" to="/automobiles/add">Add Auto to Inventory</NavLink>
+              <NavLink className="nav-link" to="/automobiles/add">Add Auto to Inventory</NavLink>
             </li>
           </ul>
         </div>
@@ -76,4 +76,3 @@ function Nav() {
 }
 
 export default Nav;
-
